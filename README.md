45ea49f8d9ea8b8b83696874a65805c34270397c
Tamás Varga <varga.tamas.pro@gmail.com>
2026-05-29 15:10:37 +0200

    feat: read input CSV file

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # cup-results-calculator
 
 Simple Python script which calculates the results of a cup competition
+
+## Running the script
+
+The script needs to be invoked by providing the path and name of an input file as argument. This input file is the CSV formatted results of a competition, exported from FjwW application.
+
+```sh
+python calculate_cup_results.py individual_results.csv
+```
--- calculate_cup_results.py
+++ calculate_cup_results.py
@@ -1,8 +1,14 @@
-def read_csv_file_of_individual_results():
-    pass
+import csv
+import sys
 
-def parse_csv_to_competitors():
-    pass
+def read_csv_file_of_individual_results(filename_with_path):
+    competitors = []
+    with open(filename_with_path, "r", encoding="utf-8") as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=";")
+        for row in csv_reader:
+            competitors.append(row)
+    print(f"Read {len(competitors)} competitors from input CSV file")
+    return competitors
 
 def assign_points_to_competitors():
     pass
@@ -17,8 +23,10 @@ def render_results():
     pass
 
 if __name__ == "__main__":
-    read_csv_file_of_individual_results()
-    parse_csv_to_competitors()
+    if len(sys.argv) < 2:
+        raise Exception("Missing argument: path and name of CSV input file containing the individual results")
+    filename_with_path = sys.argv[1]
+    competitors = read_csv_file_of_individual_results(filename_with_path)
     assign_points_to_competitors()
     assign_points_to_clubs()
     rank_clubs()
